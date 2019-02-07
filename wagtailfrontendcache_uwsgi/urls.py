182cad2044b7d8ef3d85ee2b81e8694421e9b6d4
Mario César Señoranis Ayala <mariocesar.c50@gmail.com>
2019-02-07 11:21:33 -0400

    Tweak wagtail integration

--- wagtailfrontendcache_uwsgi/urls.py
+++ wagtailfrontendcache_uwsgi/urls.py
@@ -3,17 +3,17 @@ from django.contrib import admin
 
 from . import views
 
-appname = 'wagtailfrontendcache_uwsgi'
+app_name = 'wagtailfrontendcache_uwsgi'
 
 urlpatterns = [
     url(
         r"^$",
         admin.site.admin_view(views.DashboardView.as_view()),
-        name="wagtail_cache_setup",
+        name="setup",
     ),
     url(
         r"^flush/$",
-        admin.site.admin_view(views.FlushCacheView.as_view()),
-        name="wagtail_cache_setup",
+        admin.site.admin_view(views.flush_cache),
+        name="flush",
     )
 ]
--- wagtailfrontendcache_uwsgi/wagtail_hooks.py
+++ wagtailfrontendcache_uwsgi/wagtail_hooks.py
@@ -1,7 +1,24 @@
 from django.conf.urls import url, include
+from django.urls import reverse_lazy
+from wagtail.admin.menu import MenuItem
 from wagtail.core import hooks
 
 from . import urls as wagtailfrontendcache_uwsgi_urls
+
+
+class SetupMenuItem(MenuItem):
+    def is_shown(self, request):
+        return request.user.is_staff
+
+
+@hooks.register("register_settings_menu_item")
+def register_uwsgi_menu_item():
+    return SetupMenuItem(
+        "Frontend Cache",
+        reverse_lazy("wagtailfrontendcache_uwsgi:setup"),
+        classnames="icon icon-cogs",
+        order=800,
+    )
 
 
 @hooks.register("register_admin_urls")
@@ -9,6 +26,6 @@ def register_admin_urls():
     return [
         url(
             r"^wagtailfrontendcache_uwsgi/",
-            include(wagtailfrontendcache_uwsgi_urls),
+            include(wagtailfrontendcache_uwsgi_urls, namespace='wagtailfrontendcache_uwsgi'),
         )
     ]
